436ca7dfc2f6d485f330044dc2e227f115383fe3
Manank Patel <manank@simplior.com>
2023-05-12 18:49:22 +0530

    Working On Full Screen

--- src/main/main.ts
+++ src/main/main.ts
@@ -71,6 +71,7 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
+    // fullscreen: true,
     width: 1024,
     height: 728,
     icon: getAssetPath('icon.png'),
@@ -83,6 +84,11 @@ const createWindow = async () => {
 
   mainWindow.loadURL(resolveHtmlPath('index.html'));
 
+  mainWindow.on('unmaximize', (event) => {
+    console.log("Called");
+    event.preventDefault();
+  })
+
   mainWindow.on('ready-to-show', () => {
     if (!mainWindow) {
       throw new Error('"mainWindow" is not defined');
@@ -90,6 +96,7 @@ const createWindow = async () => {
     if (process.env.START_MINIMIZED) {
       mainWindow.minimize();
     } else {
+      mainWindow.maximize();
       mainWindow.show();
     }
   });
